51b7e03ef3331800b478f4545b296cfeab3af1fa
JoWonYeong <wswy17@gmail.com>
2023-03-07 23:54:38 +0900

    components and props

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Avatar from './Avatar';
 
-export default function Profile({ img, name, title }) {
+export default function Profile({ img, name, title, isNew }) {
   return (
     <>
       <div className='profile'>
-        <img className='photo' src={img} alt='avatar' />
+        <Avatar img={img} isNew={isNew} />
         <h1>{name}</h1>
         <p>{title}</p>
       </div>
