28cc8acf0ae4ba099f28568369ee99fdaebb3e31
JoWonYeong <wswy17@gmail.com>
2023-03-09 23:23:43 +0900

    props로 부모의 state 변경하는 함수 전달해서 자식에게  event 발생했을 때 함수실행

--- basic/src/AppCounter.jsx
+++ basic/src/AppCounter.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
 export default function AppCounter() {
+  let [count, setCount] = useState(0);
+  // const onClickHandler = () => {
+  //   setCount(count + 1);
+  // };
+  const onClickHandler = () => setCount((prev) => prev + 1);
+
   return (
     <>
-      <Counter />
+      <div className='container'>
+        <div className='banner'>
+          Total Count: {count} {count > 10 ? '🔥' : '❄️'}
+        </div>
+      </div>
+      <div className='counters'>
+        <Counter total={count} onClickHandler={onClickHandler} />
+        <Counter total={count} onClickHandler={onClickHandler} />
+      </div>
     </>
   );
 }
--- basic/src/components/Counter.jsx
+++ basic/src/components/Counter.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 
-export default function Counter() {
+export default function Counter({ total, onClickHandler }) {
   const [count, setCount] = useState(0);
   return (
     <>
       <div className='counter'>
-        <span className='number'>{count}</span>
+        <p className='number'>
+          {count}
+          <span className='total'>/{total}</span>
+        </p>
         <button
           className='button'
           onClick={() => {
+            // n번 써도 count + 1
             // setCount(count + 1);
-            setCount((prevCount) => {
-              return prevCount + 1;
-            });
+            // n번 쓰면 count + n
+            // setCount((prevCount) => { return prevCount + 1; });
             setCount((prevCount) => prevCount + 1);
+            onClickHandler();
           }}
         >
           Add +
